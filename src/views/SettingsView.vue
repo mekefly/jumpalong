7c93e13aaa5db9f930b3c47b5c0e0fcd46f9f66e
meke <3197801970@qq.com>
2023-03-23 00:47:48 +0800

    feat: 增加清楚本地存储选项

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,7 +1,22 @@
 <script lang="ts" setup>
 import { config } from "@/nostr/nostr";
 import ArrowForwardIosRoundVue from "../components/icon/ArrowForwardIosRound.vue";
-import { clearCache } from "../utils/cache/index";
+import { clearCache, intelligentCleaning } from "../utils/cache/index";
+const dialog = useDialog();
+function handelClearLocalStorage() {
+  dialog.warning({
+    title: "警告",
+    content:
+      "您确定要清理localStorage吗？localStorage是浏览器提供的本地存储，您需要备份一下私钥和稍微记忆一下中续，否则此账户将无法登录",
+    positiveText: "确定",
+    negativeText: "不确定",
+    onPositiveClick: () => {
+      localStorage.clear();
+      location.reload();
+    },
+    onNegativeClick: () => {},
+  });
+}
 </script>
 
 <template>
@@ -14,9 +29,10 @@ import { clearCache } from "../utils/cache/index";
         <n-icon><ArrowForwardIosRoundVue /> </n-icon>
       </template>
     </n-list-item>
-    <n-list-item @click="clearCache">清楚垃圾缓存</n-list-item>
+    <n-list-item @click="intelligentCleaning">清楚垃圾缓存</n-list-item>
     <n-list-item @click="clearCache">清楚缓存</n-list-item>
-    <n-list-item @click="clearCache">
+    <n-list-item @click="handelClearLocalStorage">清楚本地存储</n-list-item>
+    <n-list-item>
       中继自动Ping
       <template #suffix>
         <n-switch v-model:value="config.autoPing" />
