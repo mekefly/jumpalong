a52c4236fea85c465130e7fbb9218c1e76ae963e
meke <3197801970@qq.com>
2023-03-23 00:55:21 +0800

    fix: 优化一下把退出登录改为对话框

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -1,15 +1,25 @@
 <script lang="ts" setup>
-import { NPopconfirm } from "naive-ui";
-import { logout } from "../api/login";
+import { logout } from "@/api/login";
+
+const dialog = useDialog();
+function handelLogout() {
+  dialog.warning({
+    title: "警告",
+    content:
+      "确认退出登录吗？您是否确认已经备份私钥?如果您没有备份私钥，您将再也无法登录此账户",
+    positiveText: "确定",
+    negativeText: "不确定",
+    onPositiveClick: () => {
+      logout();
+    },
+  });
+}
 </script>
 
 <template>
-  <n-popconfirm @positive-click="logout">
-    <template #trigger>
-      <slot>Logout</slot>
-    </template>
-    确认退出登录吗？您是否确认已经备份私钥?
-  </n-popconfirm>
+  <div @click="handelLogout">
+    <slot>Logout</slot>
+  </div>
 </template>
 
 <style scoped></style>
