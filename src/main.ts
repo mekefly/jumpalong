5f5303cd93c1564b43d1c65eeda8532ea548899a
meke <3197801970@qq.com>
2023-04-02 14:25:24 +0800

    fix: Delete useless code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import "./style.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import i18n, { t } from "./i18n";
+import i18n from "./i18n";
 import { initializeRuntime } from "./nostr/nostrRuntime";
 import router from "./router";
 
@@ -16,7 +16,4 @@ const app = createApp(App);
 
 app.use(router);
 app.use(i18n);
-app.use((app) => {
-  (app as any).t = t;
-});
 app.mount("#app");
