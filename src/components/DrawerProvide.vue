e4292ad65e18cd0418a0ceb4398ffdf18a6d2816
meke <3197801970@qq.com>
2023-04-02 14:26:28 +0800

    feat: Drawer.vue

---
+++ src/components/DrawerProvide.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { provideDrawerState } from "./Drawer";
+
+const { id } = provideDrawerState();
+</script>
+
+<template>
+  <slot :id="id"> </slot>
+</template>
+
+<style scoped></style>
