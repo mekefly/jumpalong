09ab7453bf49848ffa74891f13de201286886745
meke <3197801970@qq.com>
2023-04-05 21:18:04 +0800

    chore: Deployment Automation

--- scripts/deploy/index.ts
+++ scripts/deploy/index.ts
@@ -1,6 +1,6 @@
 import { cac } from "cac";
 import { readFileSync } from "fs";
-import { exec } from "shelljs";
+import { cd, echo, exec, exit, which } from "shelljs";
 
 //获取json
 const packageJsonString = readFileSync("package.json", "utf-8");
@@ -41,30 +41,43 @@ for (const key in Options) {
 cli.usage("可以输入参数设置\n','号将清空默认值，命令后面的例子就是默认值选项");
 cli.help();
 
-const { args, options } = cli.parse();
+console.log("You can enter --help,To get more help");
+
+// 参数
+const { options } = cli.parse();
 
 const { test, noBuild, help } = options as Partial<
   typeof Options & { help: boolean }
 >;
+
+//处理帮助
 if (help) {
 } else {
+  //验证是否具有git命令
+  if (!which("git")) {
+    echo("Sorry, this script requires git");
+    exit(1);
+  }
   deploy();
 }
-function deploy() {
-  !noBuild && exec("npm run build-only");
 
-  exec("cd dist");
+function deploy() {
+  if (!noBuild) {
+    exec("npm run build-only");
+  }
+
+  cd("dist");
 
   exec("git init");
   exec("git add -A");
   exec("git commit -m 'deploy'");
-
   exec(`git branch -M main`);
+
   if (test) {
     exec(`git push -f -u git@github.com:jumpalong/test.git main`);
   } else {
     exec(`git push -f git@github.com:jumpalong/jumpalong.github.io.git main`);
   }
 
-  exec("cd ..");
+  cd("..");
 }
